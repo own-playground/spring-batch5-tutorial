1e362d2aadc79be4d7c45ed541809ab17d9e60e9
ahn-sj <jaesa5221@gmail.com>
2024-09-15 13:32:47 +0900

    init: 프로젝트 초기 세팅

---
+++ src/test/java/com/tally/batch/BatchTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tally.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
